383fb76b584b78f8d4fe9450feca76b1e7b73aa4
Jordan Corfield <JCorfield.1999@gmail.com>
2021-10-14 13:00:42 +0100

    Add getRandomPhraseAsArray function
    Add getRandomPhraseAsArray function, which takes an array as an argument and converts one of the array's items into an array of letters.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const qwerty = document.getElementById('qwerty');
+const phrases = ['fly me to the moon', 'one small step for man', 'never gonna give you up', 'somewhere over the rainbow', 'kill two birds with one stone'];
 let phrase = document.getElementById('phrase');
 let missed = 0;
 
@@ -6,3 +7,9 @@ const startGameButton = document.querySelector('a.btn__reset');
 startGameButton.addEventListener('click', () => {
     startGameButton.parentNode.style.display = 'none';
 });
+
+function getRandomPhraseAsArray(arr) {
+    randomPhrase = arr[Math.floor(Math.random() * arr.length)];
+    randomPhraseCharacters = randomPhrase.split('');
+    return randomPhraseCharacters;
+}
